311c1dc8d6bc3a9642a060b486361fe8f5036cae
Jon Kolman <jonathankolman@gmail.com>
2017-08-14 22:03:03 -0400

    archived

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+  render() {
+    return (
+      <div className="app flex-row align-items-center">
+        hello world
+      </div>
+    )
+  }
+}
+
+export default Login;
